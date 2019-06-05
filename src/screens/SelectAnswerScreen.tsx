f24c5bdc0fb7b4cdacc1484fb244052983c3f568
FakeShemp <sakcheen@gmail.com>
2019-06-05 13:02:48 +0200

    Add redirecting to dashboard

--- src/screens/SelectAnswerScreen.tsx
+++ src/screens/SelectAnswerScreen.tsx
@@ -3,13 +3,13 @@ import { Button, Card, ListGroup } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ContainerComponent from '../components/ContainerComponent';
 import { GetSpotifyInfo } from '../components/HOCS/GetSpotifyInfoHoc';
-import {SpinnerComponent} from '../components/SpinnerComponent';
+import { SpinnerComponent } from '../components/SpinnerComponent';
 import CustomMediaComponent from '../components/AudioPlayerComponent'
 import './SelectAnswerScreen.css'
 
 interface Props {
     getSong: any
-}   
+}
 
 interface State {
     id: string,
@@ -29,7 +29,7 @@ interface State {
             release_date: string,
             name: string
         },
-        preview_url:string
+        preview_url: string
     }
 }
 
@@ -54,7 +54,7 @@ class SelectAnswerScreen extends Component<Props, State>{
                     release_date: "",
                     name: ""
                 },
-                preview_url:""
+                preview_url: ""
             }
         }
     }
@@ -66,91 +66,88 @@ class SelectAnswerScreen extends Component<Props, State>{
 
         this.props.getSong(finalTrackID)
             .then((res: any) => {
-                this.setState({ track: res,success: true })
+                this.setState({ track: res, success: true })
             });
     }
 
     render() {
-
         const artist = this.state.track.artists[0].name;
         const songTitle = this.state.track.name;
         const albumTitle = this.state.track.album.name;
-        const albumYear = this.state.track.album.release_date.slice(0,4);
+        const albumYear = this.state.track.album.release_date.slice(0, 4);
 
         return (
             <Fragment>
-            {this.state.success &&
-            <ContainerComponent>
-                <Card>
-                    <Card.Body>
-                        <Card.Title className="text-center">Select question answer</Card.Title>
-                       
-                        <ListGroup variant="flush">
-                        <ListGroup.Item className="player listItem" >
-                            <p className="playerText">Preview Song</p> 
-                        <CustomMediaComponent songPreview={this.state.track.preview_url}>
-                        </CustomMediaComponent>
-                        </ListGroup.Item>
-                            <ListGroup.Item className="listItem">
-                                <Link to={{
-                                    pathname: "/questionform",
-                                    state: {
-                                        answer: artist
-                                    }
-                                }} >
-                                    <Button className="AnswerButton">
-                                        <h6>Artist</h6>
-                                        <h4>{artist}</h4>
-                                    </Button>
-                                </Link>
-                            </ListGroup.Item>
-                            <ListGroup.Item className="listItem">
-                                <Link to={{
-                                    pathname: "/questionform",
-                                    state: {
-                                        answer: songTitle
-                                    }
-                                }} >
-                                    <Button className="AnswerButton">
-                                        <h6>Song Title</h6>
-                                        <h4>{songTitle}</h4>
-                                    </Button>
-                                </Link>
-                            </ListGroup.Item>
-                            <ListGroup.Item className="listItem">
-                                <Link to={{
-                                    pathname: "/questionform",
-                                    state: {
-                                        answer: albumTitle
-                                    }
-                                }} >
-                                    <Button className="AnswerButton">
-                                        <h6>Album Title</h6>
-                                        <h4>{albumTitle}</h4>
-                                    </Button>
-                                </Link>
-                            </ListGroup.Item>
-                            <ListGroup.Item className="listItem">
-                                <Link to={{
-                                    pathname: "/questionform",
-                                    state: {
-                                        answer: albumYear
-                                    }
-                                }} >
-                                    <Button className="AnswerButton">
-                                        <h6>Release Year</h6>
-                                        <h4>{albumYear}</h4>
-                                    </Button>
-                                </Link>
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Card.Body>
-                </Card>
-            </ContainerComponent>
-        }
-        {!this.state.success && <SpinnerComponent/>}
-        </Fragment>
-        
+                {this.state.success &&
+                    <ContainerComponent>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title className="text-center">Select question answer</Card.Title>
+                                <ListGroup variant="flush">
+                                    <ListGroup.Item className="player listItem" >
+                                        <p className="playerText">Preview Song</p>
+                                        <CustomMediaComponent songPreview={this.state.track.preview_url}>
+                                        </CustomMediaComponent>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item className="listItem">
+                                        <Link to={{
+                                            pathname: "/questionform",
+                                            state: {
+                                                answer: artist
+                                            }
+                                        }} >
+                                            <Button className="AnswerButton">
+                                                <h6>Artist</h6>
+                                                <h4>{artist}</h4>
+                                            </Button>
+                                        </Link>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item className="listItem">
+                                        <Link to={{
+                                            pathname: "/questionform",
+                                            state: {
+                                                answer: songTitle
+                                            }
+                                        }} >
+                                            <Button className="AnswerButton">
+                                                <h6>Song Title</h6>
+                                                <h4>{songTitle}</h4>
+                                            </Button>
+                                        </Link>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item className="listItem">
+                                        <Link to={{
+                                            pathname: "/questionform",
+                                            state: {
+                                                answer: albumTitle
+                                            }
+                                        }} >
+                                            <Button className="AnswerButton">
+                                                <h6>Album Title</h6>
+                                                <h4>{albumTitle}</h4>
+                                            </Button>
+                                        </Link>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item className="listItem">
+                                        <Link to={{
+                                            pathname: "/questionform",
+                                            state: {
+                                                answer: albumYear
+                                            }
+                                        }} >
+                                            <Button className="AnswerButton">
+                                                <h6>Release Year</h6>
+                                                <h4>{albumYear}</h4>
+                                            </Button>
+                                        </Link>
+                                    </ListGroup.Item>
+                                </ListGroup>
+                            </Card.Body>
+                        </Card>
+                    </ContainerComponent>
+                }
+                {!this.state.success && <SpinnerComponent />}
+            </Fragment>
         )
     }
 }
